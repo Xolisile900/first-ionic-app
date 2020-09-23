e3683e85f45dbd68d71ab90d40ed43f9b7e7dee2
Xolisile900 <j.x.ubisi@gmail.com>
2020-09-23 02:24:19 +0200

    3 files modified

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UpdateComponent } from '../update/update.component';
 
 @Component({
   selector: 'app-todo',
@@ -15,31 +16,64 @@ export class TodoComponent implements OnInit {
   addClicked: false;
   inputValue: string="";
 
+  listName: any = '';
+  arrayList = [];
+  todoArr = [];
+  updatelist: any = '';
+
+addTask() {
+
+  if (this.listName.length > 0) {
+     let task = this.listName;
+     this.arrayList.push(task);
+     console.log(this.listName);
+
+   //  this.todoArr = [
+   //   {contentValue: listName, statu: " ", priority: false},
+   //  ];
+
+     this.listName = '';
+   }
+}
+
+deleteTask(index) {
+    this.status = "done";
+    if (this.status == "done"){
+      this.arrayList.splice(index, 1);
+      this.priority = true;
+    }
+  }
+
+  updateTask(index){
+    //this.updatelist =
+    this.arrayList = this.updatelist;
+  }
+
   todoArr = [
    {contentValue: " ", statu: " ", priority: false},
   ];
 
   
-  inputList(){
-    this.contentValue = this.inputValue;
-    console.log(this.inputValue);
-    console.log(this.contentValue);
-  }
+ // inputList(){
+   // this.contentValue = this.inputValue;
+    //console.log(this.inputValue);
+    //console.log(this.contentValue);
+  //}
 
  // console.log(this.inputValue);
 
-  addToList() {
-    this.contentValue = this.inputValue;
-    this.todoArr.push(this.contentValue);//, this.status, this.priority);
+  //addToList() {
+    //this.contentValue = this.inputValue;
+    //this.todoArr.push(this.contentValue);//, this.status, this.priority);
     //this.model = {};
-    console.log(this.contentValue);
-  }
+   // console.log(this.contentValue);
+  //}
 
-  lableText: string = "";
-    inputValueToLable(){
-      console.log(this.inputValue);
-      this.lableText = this.inputValue;
-    }
+  //lableText: string = "";
+    //inputValueToLable(){
+      //console.log(this.inputValue);
+      //this.lableText = this.inputValue;
+    //}
 
   addClickedbtn() {
     this.addClicked = !this.addClicked;
@@ -48,6 +82,9 @@ export class TodoComponent implements OnInit {
   
 
   ngOnInit() {
+//this.taskName = this.id;
+console.log("task "+this.listName);
   }
+
 
 }
--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoComponent } from '../todo/todo.component';
 
 @Component({
   selector: 'app-update',
@@ -9,6 +10,19 @@ export class UpdateComponent implements OnInit {
 
   constructor() { }
 
+  tList = TodoComponent;
+
+  itemlist = this.tList;
+
+
+  update() {
+    let edit = this.tList;
+    for(let i = 0; i < this.tList.length; i++) {
+
+        this.tList[i] = this.itemList.arrayList;
+
+    }
+  }
 
   closeModal(){
     
